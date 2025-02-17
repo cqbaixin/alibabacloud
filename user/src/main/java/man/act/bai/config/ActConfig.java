be8fc131c6458efd0a2ab78959657ffce07af145
act <act@act.com>
2025-02-17 23:05:33 +0800

    config

---
+++ user/src/main/java/man/act/bai/config/ActConfig.java
@@ -0,0 +1,42 @@
+package man.act.bai.config;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix ="act.bai" )
+@RefreshScope
+public class ActConfig {
+
+    @NacosValue(value = "${name:default_url}", autoRefreshed = true)
+    private  String name;
+
+    private  String tel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "ActConfig{" +
+                "name='" + name + '\'' +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
+}
--- user/src/main/java/man/act/bai/controller/IndexController.java
+++ user/src/main/java/man/act/bai/controller/IndexController.java
@@ -1,5 +1,6 @@
 package man.act.bai.controller;
 
+import man.act.bai.config.ActConfig;
 import man.act.bai.feign.order.OrderFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +11,23 @@ public class IndexController {
 
     @Autowired
     public OrderFeignClient orderFeignClient;
+
+    @Autowired
+    public ActConfig actConfig;
     @GetMapping(path = "/")
-    public String index()
-    {
+    public String index() {
         return "welcome,User";
     }
 
     @GetMapping(path = "/user/order")
 
-    public String order()
-    {
-        return  "userIndex:"+orderFeignClient.index();
+    public String order() {
+        return "userIndex:" + orderFeignClient.index();
+    }
+
+
+    @GetMapping(path = "/config")
+    public String config(){
+        return  actConfig.toString();
     }
 }
