6d1a19cbd1c759d0cd67c027ced77b3d2007c1e0
act <act@act.com>
2025-02-17 22:02:02 +0800

    init

--- order/src/main/java/man/act/bai/controller/IndexController.java
+++ order/src/main/java/man/act/bai/controller/IndexController.java
@@ -1,14 +1,25 @@
 package man.act.bai.controller;
 
+import man.act.bai.feign.user.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    @Autowired
+    public UserFeignClient userFeignClient;
     @GetMapping(path = "/")
     public String index()
     {
         return "Welcome,Order";
+    }
+
+    @GetMapping(path = "/order/user")
+
+    public String user()
+    {
+        return  "orderIndex:"+userFeignClient.index();
     }
 }
--- user/src/main/java/man/act/bai/controller/IndexController.java
+++ user/src/main/java/man/act/bai/controller/IndexController.java
@@ -1,14 +1,25 @@
 package man.act.bai.controller;
 
+import man.act.bai.feign.order.OrderFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    @Autowired
+    public OrderFeignClient orderFeignClient;
     @GetMapping(path = "/")
     public String index()
     {
         return "welcome,User";
+    }
+
+    @GetMapping(path = "/user/order")
+
+    public String order()
+    {
+        return  "userIndex:"+orderFeignClient.index();
     }
 }
